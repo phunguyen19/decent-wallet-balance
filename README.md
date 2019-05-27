ef00d3637e54504bf18e0072a7ed12c4ad8acc09
Phu Nguyen <phu.nguyen19@gmail.com>
2019-05-27 14:07:44 +0700

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# get-top-10-wallets
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as http from 'http';
+
+const port = process.env.PORT || 3000;
+
+export const server = http.createServer((req, res) => {
+  res.write('Hello World!');
+  res.end();
+});
+
+server.listen(port);
